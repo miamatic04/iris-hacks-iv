3b48a0f64171c1b19cb3ede5d446a7b425fb8032
Mia <miamatic04@gmail.com>
2026-08-09 00:40:06 +0200

    Connect frontend form to backend API with test response

--- public/script.js
+++ public/script.js
@@ -5,4 +5,13 @@ rewriteBtn = document.getElementById('rewriteBtn')
 rewriteBtn.addEventListener('click', function() {
     console.log(recipe.value)
     console.log(restriction.value)
+
+    fetch('/api/rewrite', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json'},
+    body: JSON.stringify({recipe : recipe.value, restriction: restriction.value})
+    })
+        .then(res => res.json())
+        .then(data => console.log(data))
 })
+
--- server.js
+++ server.js
@@ -2,5 +2,13 @@ express = require('express')
 app = express()
 
 app.use(express.static('public'))
+app.use(express.json())
+
+app.post('/api/rewrite', function(req, res) {
+    recipe = req.body.recipe
+    restriction = req.body.restriction
+
+    res.json({received: true})
+})
 
 app.listen(3000, console.log('Server running on http://localhost:3000'))
